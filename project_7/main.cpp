692aba5fcdb21c9d7d0cfff0c4376dfb728de035
Leison Gao <leisongao2005@g.ucla.edu>
2023-11-29 16:13:09 -0800

    Initial Commit

---
+++ project_7/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  project_7
+//
+//  Created by Leison Gao on 11/29/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
